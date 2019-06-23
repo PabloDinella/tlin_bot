277082df7da163f8142da7ba54a17ec1746d4333
Pablo Rocha Dinella <pablo.dinella@gmail.com>
2019-06-22 22:53:05 -0300

    remove audio download into file, now it sends the audio directly from origin

--- bot.js
+++ bot.js
@@ -11,17 +11,10 @@ const channelID = process.env.channelID;
 
 request({
   uri: "http://www.thelordisnear.ca/",
-}, function(error, response, body) {
+}, function (error, response, body) {
 
   const parsed = parse(body)
 
-  var file = fs.createWriteStream(path.join(__dirname, 'mp3', parsed.date+'.mp3'));
-  request.get(`http://growingrace.com/mp3s/TheLordIsNear/${parsed.date.split('-')[2]}/${parsed.date}.mp3`).pipe(file);
-
-  file.on('finish', () => {
-    bot.sendMessage(channelID, parsed.message)
-    bot.sendAudio(channelID, path.join(__dirname, 'mp3', parsed.date+'.mp3'))
-  })
-
-  // TODO: delete audio file or not even save it, just stream to the channel?
+  bot.sendMessage(channelID, parsed.message)
+  bot.sendAudio(channelID, `http://growingrace.com/mp3s/TheLordIsNear/${parsed.date.split('-')[2]}/${parsed.date}.mp3`)
 });
