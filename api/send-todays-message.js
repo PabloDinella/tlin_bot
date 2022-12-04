be2990679df217097001587eaee5d51968ed7fef
Pablo R. Dinella <pablo.dinella@gmail.com>
2022-12-04 13:10:24 -0300

    back do js

--- api/send-todays-message.js
+++ api/send-todays-message.js
@@ -1,4 +1,4 @@
-import { run } from "../src/run";
+import { run } from "../src/run.js";
 
 export default async function handler(request, response) {
   const result = await run(
---
+++ src/run.js
@@ -0,0 +1,86 @@
+import parse from "./parseMessage.js";
+import fetch from "node-fetch";
+
+const createBot = (token) => {
+  const telegramApiUrl = `https://api.telegram.org/bot${token}`;
+
+  return {
+    sendMessage: async (channelId, text) => {
+      const response = await fetch(
+        telegramApiUrl +
+          "/sendMessage?" +
+          new URLSearchParams({
+            chat_id: channelId,
+            text,
+          }),
+        {
+          method: "POST",
+        }
+      );
+
+      console.log("telegram's response", await response.json());
+      return response;
+    },
+    sendAudio: async (channelId, audio) => {
+      const response = await fetch(
+        telegramApiUrl +
+          "/sendAudio?" +
+          new URLSearchParams({
+            chat_id: channelId,
+            audio,
+          }),
+        {
+          method: "POST",
+        }
+      );
+
+      console.log("telegram's response", await response.json());
+      return response;
+    },
+  };
+};
+
+export async function run({ mode, channelId, channelIdTesting, token }) {
+  fetch("https://webhook.site/22a0b275-3692-4e67-a693-71ec6458ccfd", {
+    method: "post",
+    body: "testt",
+  });
+  const bot = createBot(token);
+
+  const body = await fetch("http://www.thelordisnear.ca/", {
+    method: "get",
+  }).then((response) => response.text());
+
+  const parsed = parse(body);
+
+  console.log(`Running in mode: ${mode}`);
+
+  console.log(parsed);
+
+  if (mode === "production") {
+    await bot.sendMessage(channelId, parsed.message);
+    await bot.sendAudio(
+      channelId,
+      `http://growingrace.com/mp3s/TheLordIsNear/${parsed.date.split("-")[2]}/${
+        parsed.date
+      }.mp3`
+    );
+    return true;
+  }
+
+  if (mode === "testing") {
+    await bot.sendMessage(channelIdTesting, parsed.message);
+    await bot.sendAudio(
+      channelIdTesting,
+      `http://growingrace.com/mp3s/TheLordIsNear/${parsed.date.split("-")[2]}/${
+        parsed.date
+      }.mp3`
+    );
+    throw new Error("Chegou aqui");
+    return true;
+  }
+
+  if (mode === "logonly") {
+    return true;
+  }
+}
